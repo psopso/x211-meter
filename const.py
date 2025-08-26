0f0d8d5531309fec46c3ecc61af3d2c30e1d1465
psopso <57403373+psopso@users.noreply.github.com>
2025-08-26 22:19:46 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,10 @@
+DOMAIN = "xt211"
+
+SENSOR_MAP = {
+    "1.8.0": {"name": "Total Energy", "unit": "kWh"},
+    "1.8.1": {"name": "Tariff 1 Energy", "unit": "kWh"},
+    "1.8.2": {"name": "Tariff 2 Energy", "unit": "kWh"},
+    "1.8.3": {"name": "Tariff 3 Energy", "unit": "kWh"},
+    "1.8.4": {"name": "Tariff 4 Energy", "unit": "kWh"},
+    "datetime": {"name": "Last Update", "unit": None},
+}
