88b6f7f1bfac0ff26c8f06abf00cd57bdb73e17f
psopso <57403373+psopso@users.noreply.github.com>
2025-11-21 21:26:12 +0100

    Update mqtt_handler.py

--- custom_components/xt211/mqtt_handler.py
+++ custom_components/xt211/mqtt_handler.py
@@ -1,7 +1,11 @@
-async def handle_status(hass, payload: str):
+async def handle_status(hass, payload: str, config):
     # TODO: vlastní logika zpracování statusu
+    influx_host = config.get("influxdb_host")
+    influx_port = config.get("influxdb_port")
     pass
 
-async def handle_data(hass, payload: str):
+async def handle_data(hass, payload: str, config):
     # TODO: vlastní logika zpracování dat
+    influx_host = config.get("influxdb_host")
+    influx_port = config.get("influxdb_port")
     pass
