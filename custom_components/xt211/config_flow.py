84f31474a7b35a40230dbc168ab082fe641a64a0
psopso <57403373+psopso@users.noreply.github.com>
2025-12-10 07:53:28 +0100

    Update config_flow.py

--- custom_components/xt211/config_flow.py
+++ custom_components/xt211/config_flow.py
@@ -56,7 +56,7 @@ class Xt211OptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry):
         """Initialize options flow."""
-        self.config_entry = config_entry
+        self._config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
